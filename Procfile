c2eaa7eccbdd532118ce3d80b35068dfd4bc4d14
BuildTools <cephas8080@outlook.com>
2022-05-03 21:09:57 +0100

    first

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: npm run deploy && node dist/index.js
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('the skid defender api!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
