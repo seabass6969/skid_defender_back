c0c91d8fd33b2f1693f13670c6f2d2669f6e4145
BuildTools <cephas8080@outlook.com>
2022-05-08 22:51:52 +0100

    crashing issue 2

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ import {DB_connect} from './dbsetup';
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+  res.send('the skid defender api! no more DOC')
+})
 
 app.get('/api/listall_MC', async (req, res) => {
     res.send(JSON.stringify(await DB_connect()))
